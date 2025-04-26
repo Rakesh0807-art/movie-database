71a04dcd7646c1b38aabac2778b045cdbd8d1d50
RAKESH <sampathraddyas@gmail.com>
2025-04-26 22:13:17 +0530

    Update vite.config.js

--- mern-netflix-clone-master/frontend/vite.config.js
+++ mern-netflix-clone-master/frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  base: "/movie-database/", // ✨ Add this line (VERY IMPORTANT)
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
 });
